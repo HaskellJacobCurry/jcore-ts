a561cb15ebc75f81a016efd45ae4d63e9fdbcf5c
Gogs <gogs@fake.local>
2021-06-28 23:41:44 +0800

    p

---
+++ extension/haskell/test/src/Clojure/LazySequence.ts
@@ -0,0 +1,61 @@
+import {LazySequence} from '../../../dist/Clojure/LazySequence'
+import {Int} from '../../../dist/Data/Int'
+import {Unit} from '../../../dist/Data/Unit'
+import {Bool} from '../../../dist/Data/Bool'
+import {
+    trampoline,
+    apply,
+} from '../../../dist/util'
+
+({
+    0: () => (
+        apply({
+            lazy: (
+                apply(
+                    LazySequence<Int>(Int.Ring.add(Int(1)))(Int(1))
+                )(_ => apply(
+                    LazySequence.filter<Int>(Int.odd)(_)
+                ))(_ => LazySequence.fmap(Int.Ring.mul(Int(3)))(_))
+            )
+        })(({lazy}) => (
+            apply(
+                trampoline<Unit>()((lazy: LazySequence<Int>) => next => (
+                    Int.Ord.lt(lazy.value)(Int(45)).cata({
+                        True: () => (
+                            console.log({v: Int.Show.show(lazy.value).toString()}),
+                            next(lazy.next())
+                        ),
+                        False: () => (
+                            console.log('end'),
+                            console.log({v: Int.Show.show(lazy.value).toString()}),
+                            Unit()
+                        )
+                    })
+                ))
+            )(_ => _(lazy))
+        ))
+    ),
+    1: () => (
+        apply({
+            lazy: (
+                apply(
+                    LazySequence<Int>(Int.Ring.add(Int(1)))(Int(1))
+                )(_ => apply(
+                    LazySequence.filter<Int>(Int.odd)(_)
+                ))(_ => apply(
+                    LazySequence.fmap(Int.Ring.mul(Int(3)))(_)
+                ))(_ => (
+                    //LazySequence.take(Int(0))(_)
+                    LazySequence.until<Int>(_ => Bool.fromI(Int.Ord.lt(Int(66))(_)))(_)
+                ))
+            )
+        })(({lazy}) => (
+            apply(
+                LazySequence.evaluate<Int>(_ => (
+                    console.log(Int.Show.show(_).toString()),
+                    Unit()
+                ))
+            )(_ => _(lazy))
+        ))
+    )
+})[1]();
---
+++ extension/haskell/tmp/dist/LazySequence.test.js
@@ -0,0 +1,31 @@
+"use strict";
+exports.__esModule = true;
+var LazySequence_1 = require("./LazySequence");
+var Int_1 = require("../../dist/Data/Int");
+var Unit_1 = require("../../dist/Data/Unit");
+var Bool_1 = require("../../dist/Data/Bool");
+var util_1 = require("../../dist/util");
+({
+    0: function () { return (util_1.apply({
+        lazy: (util_1.apply(LazySequence_1.LazySequence(Int_1.Int.Ring.add(Int_1.Int(1)))(Int_1.Int(1)))(function (_) { return util_1.apply(LazySequence_1.LazySequence.filter(Int_1.Int.odd)(_)); })(function (_) { return LazySequence_1.LazySequence.fmap(Int_1.Int.Ring.mul(Int_1.Int(3)))(_); }))
+    })(function (_a) {
+        var lazy = _a.lazy;
+        return (util_1.apply(util_1.trampoline()(function (lazy) { return function (next) { return (Int_1.Int.Ord.lt(lazy.value)(Int_1.Int(45)).cata({
+            True: function () { return (console.log({ v: Int_1.Int.Show.show(lazy.value).toString() }),
+                next(lazy.next())); },
+            False: function () { return (console.log('end'),
+                console.log({ v: Int_1.Int.Show.show(lazy.value).toString() }),
+                Unit_1.Unit()); }
+        })); }; }))(function (_) { return _(lazy); }));
+    })); },
+    1: function () { return (util_1.apply({
+        lazy: (util_1.apply(LazySequence_1.LazySequence(Int_1.Int.Ring.add(Int_1.Int(1)))(Int_1.Int(1)))(function (_) { return util_1.apply(LazySequence_1.LazySequence.filter(Int_1.Int.odd)(_)); })(function (_) { return util_1.apply(LazySequence_1.LazySequence.fmap(Int_1.Int.Ring.mul(Int_1.Int(3)))(_)); })(function (_) { return (
+        //LazySequence.take(Int(0))(_)
+        LazySequence_1.LazySequence.until(function (_) { return Bool_1.Bool.fromI(Int_1.Int.Ord.lt(Int_1.Int(1e6))(_)); })(_)); }))
+    })(function (_a) {
+        var lazy = _a.lazy;
+        return (util_1.apply(LazySequence_1.LazySequence.evaluate(function (_) { return (
+        //console.log(Int.Show.show(_).toString()),
+        Unit_1.Unit()); }))(function (_) { return _(lazy); }));
+    })); }
+})[1]();
